612d847dfc3b8109e8adbba4221255d1cd0712f1
PC_ROMAIN\Romain Loiseau <romain.loiseau@polytechnique.edu>
2017-03-10 14:15:10 +0100

    Images_prot

--- ProteinConfigStats/source/SEProteinConfigStatsApp.cpp
+++ ProteinConfigStats/source/SEProteinConfigStatsApp.cpp
@@ -8,6 +8,8 @@
 #include "SBResidue.hpp"
 #include "SBBackbone.hpp"
 #include "SBDocument.hpp"
+#include <qimage.h>
+
 
 SEProteinConfigStatsApp::SEProteinConfigStatsApp() {
 
@@ -29,7 +31,7 @@ void SEProteinConfigStatsApp::analyse(int numberOfResidues, int step, std::strin
 
 	for (int num = 1; num < 2; num++){
 
-		std::string proteinName = (QString::number(num).toStdString() + "YRF");
+		std::string proteinName = (QString::number(num).toStdString() + "AF6");
 		std::string fileName = (rpath + "\\" + proteinName + ".pdb");
 
 		// creation of a new layer
@@ -49,43 +51,57 @@ void SEProteinConfigStatsApp::analyse(int numberOfResidues, int step, std::strin
 		SBNodeIndexer nodeIndexer;
 		newLayer->getNodes(nodeIndexer, SBNode::IsType(SBNode::Residue));
 
-		//QString title = "Information";
-		//QString text = QString::number(nodeIndexer.size());
-		//SAMSON::informUser(title, text);
-
 		for (int s = 1; s < step + 1; s++){
 
 			for (int offset = 0; offset <= nodeIndexer.size() - numberOfResidues*s; offset++){
 
-				std::string n = (wpath + "\\" + proteinName + "_step" + QString::number(s).toStdString() + "_offset" + QString::number(offset).toStdString() + ".txt");
-				ofstream fichier(n, ios::out | ios::trunc);
+				std::string path_chain = (wpath + "\\"+"CHAINES" + "\\" + proteinName + "_step" + QString::number(s).toStdString() + "_offset" + QString::number(offset).toStdString() + ".png");
+				std::string path_dist = (wpath + "\\" + "IMAGES" + "\\" + proteinName + "_step" + QString::number(s).toStdString() + "_offset" + QString::number(offset).toStdString() + ".png");
+				
+				QString nom_dist = QString::fromStdString(path_dist);
+				QString nom_chain = QString::fromStdString(path_chain);
 
-				fichier << "protein " << proteinName << endl;
-				fichier << "step " << s << endl;
-				fichier << "offset " << offset << endl;
+				int size = numberOfResidues;
+				QImage image_dist = QImage(size, size, QImage::Format_RGB32);
+				QImage image_chain = QImage(size, size, QImage::Format_RGB32);
 
-				for (int A1 = offset; A1 < offset + numberOfResidues*s; A1 = A1 + s){
-					fichier << static_cast<SBResidue*>(nodeIndexer[A1])->getResidueType() << " ";
-				}
+				//ofstream fichier(path_chain, ios::out | ios::trunc);
+				//fichier << "protein " << proteinName << endl;
+				//fichier << "step " << s << endl;
+				//fichier << "offset " << offset << endl;
 
-				fichier << endl;
+				//for (int A1 = offset; A1 < offset + numberOfResidues*s; A1 = A1 + s){
+				//	fichier << static_cast<SBResidue*>(nodeIndexer[A1])->getResidueType() << " ";
+				//}
 
+				//fichier << endl;
+
+				int px = 0;
 				for (int A1 = offset; A1 < offset + numberOfResidues*s; A1 = A1 + s){
 					SBAtom* carb1 = static_cast<SBResidue*>(nodeIndexer[A1])->getBackbone()->getAlphaCarbon();
 
 					SBPosition3 pos1 = carb1->getPosition();
-
-					for (int A2 = A1 + s; A2 < offset + numberOfResidues*s; A2 = A2 + s){
+					int py = 0;
+					for (int A2 = offset; A2 < offset + numberOfResidues*s; A2 = A2 + s){
 						SBAtom* carb2 = static_cast<SBResidue*>(nodeIndexer[A2])->getBackbone()->getAlphaCarbon();
 
 						SBPosition3 pos2 = carb2->getPosition();
 
 						SBQuantity::angstrom distance = (pos1 - pos2).norm();
-						fichier << distance.getValue() << " ";
+						//fichier << distance.getValue() << " ";
+
+						int c_dist = (int)distance.getValue();
+						int c_chain = (int) ((static_cast<SBResidue*>(nodeIndexer[A1])->getResidueType())*255/26);
+						image_dist.setPixel(px, py, qRgb(c_dist,c_dist,c_dist));
+						image_dist.setPixel(px, py, qRgb(c_chain, c_chain, c_chain));
+						py++;
 					}
-					fichier << endl;
+					//fichier << endl;
+					px++;
 				}
-				fichier.close();
+				//fichier.close();
+				image_dist.save(nom_dist,0,100);
+				image_chain.save(nom_chain, 0, 100);
 			}
 		}
 
@@ -94,4 +110,5 @@ void SEProteinConfigStatsApp::analyse(int numberOfResidues, int step, std::strin
 	QString title = "Information";
 	QString text = "Computation finished";
 	SAMSON::informUser( title , text);
+
 }
